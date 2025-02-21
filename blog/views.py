603d8d416d98a2272f43ba456b50f3af838f2473
roseofvoid <bejr.dominik@seznam.cz>
2025-02-21 15:35:47 +0100

    Pagination Error Hanlding

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3) # 3 Posts per Page
     page_number = request.GET.get('page', 1) # Default page = 1
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     context = {'posts': posts}
     return render(request, 'blog/post/list.html', context)
 
